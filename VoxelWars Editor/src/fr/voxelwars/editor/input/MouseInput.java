a60a12597379c1e9de68c8b018c9f2e94db23b48
Neutron Stars <alan.vion.62.av@gmail.com>
2017-02-06 20:25:16 +0100

    Optimization Input

--- VoxelWars Editor/src/fr/voxelwars/editor/input/MouseInput.java
+++ VoxelWars Editor/src/fr/voxelwars/editor/input/MouseInput.java
@@ -7,16 +7,10 @@ public class MouseInput {
 	private static final boolean[] MOUSE_PRESSED = new boolean[Mouse.getButtonCount()];
 	
 	public static boolean isButtonDown(int button){
-		if(Mouse.isButtonDown(button) && !MOUSE_PRESSED[button]){
-			MOUSE_PRESSED[button] = true;
-			return true;
-		}
-		return false;
+		return (Mouse.isButtonDown(button) && !MOUSE_PRESSED[button]) ? MOUSE_PRESSED[button] = true : false;
 	}
 	
 	public static void updateMouseInput(){
-		for(int i = 0; i < MOUSE_PRESSED.length; i++){
-			if(MOUSE_PRESSED[i]) MOUSE_PRESSED[i] = Mouse.isButtonDown(i);
-		}
+		for(int i = 0; i < MOUSE_PRESSED.length; i++) if(MOUSE_PRESSED[i]) MOUSE_PRESSED[i] = Mouse.isButtonDown(i);
 	}
 }
